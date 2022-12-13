fdaaa23261dc52e289ebcbcb9eaee6edb83c0221
Lynn <lynn@foldr.moe>
2022-12-14 00:14:06 +0100

    Clean up rom_map.py

--- rom_map.py
+++ rom_map.py
@@ -13,11 +13,14 @@ source = "saved.hdm" if os.path.exists("saved.hdm") else "tutorial/output.hdm"
 with open(source, "rb") as f:
     patched = bytearray(f.read())
 
-def at(start, bs):
-    assert rom[start:start+len(bs)] == bs
+
+def check(start, bs):
+    assert rom[start : start + len(bs)] == bs
+
 
 def padding(start, stop, byte):
-    assert rom[start:stop] == byte * (stop-start)
+    assert rom[start:stop] == byte * (stop - start)
+
 
 def wrap(text: str):
     if text.startswith("~"):
@@ -27,6 +30,7 @@ def wrap(text: str):
     else:
         return text
 
+
 def strings(name, start, stop):
     ss = bytes(rom[start:stop]).rstrip(b" U\xe5\0").split(b"\0")
     if not os.path.exists(f"text/{name}.py"):
@@ -34,7 +38,8 @@ def strings(name, start, stop):
         for i, s in enumerate(ss):
             text = s.decode("shift-jis")
             text_r = json.dumps(text, ensure_ascii=False)
-            if i > 0: lines.append("")
+            if i > 0:
+                lines.append("")
             lines.append(f"    # {i:02x} = {text_r}")
             lines.append(f"    {text_r},")
         lines.append("]")
@@ -45,19 +50,22 @@ def strings(name, start, stop):
         translation = [wrap(s) for s in eval(code)]
         enc = ("\0".join(translation) + "\0").encode("shift-jis")
         if len(enc) > stop - start:
-            raise ValueError(f"translations too long! {name}: {len(enc)} > {stop - start}")
+            raise ValueError(
+                f"translations too long! {name}: {len(enc)} > {stop - start}"
+            )
         else:
             print(f"ok: {name}: {len(enc)} <= {stop - start}")
-        patched[start: start+len(enc)] = enc
+        patched[start : start + len(enc)] = enc
+
 
 def u32(i):
-    return struct.unpack("<L", rom[i:i+4])[0]
+    return struct.unpack("<L", rom[i : i + 4])[0]
 
 
 # There's a 512b custom boot sector that starts with a JMP instruction:
 boot_sector = rom[0x00000:0x00200]
-at(0x0, b"\xeb\x7e")
-at(0x10, b"the SWORD OF KUMDOR")
+check(0x0, b"\xeb\x7e")
+check(0x10, b"the SWORD OF KUMDOR")
 
 padding(0x0300, 0x0400, b"U")
 padding(0x0400, 0x1400, b"\xff")
@@ -81,95 +89,57 @@ gfx_maybe = rom[0x14000:0x35000]
 
 padding(0x35000, 0x40000, b"\xe5")
 
-at(0x40000, b"MAP SOLFACE")
+check(0x40000, b"MAP SOLFACE")
 map_solface = rom[0x40000:0x44000]
 
-at(0x44000, b"OUT MAP")
+check(0x44000, b"OUT MAP")
 map_out = rom[0x44000:0x48000]
 
 # Story text
-at(0x48000, "A.P.(ポーラ暦)405年".encode("shift-jis"))
-strings("intr", 0x48000, 0x491f0)
+check(0x48000, "A.P.(ポーラ暦)405年".encode("shift-jis"))
+strings("intr", 0x48000, 0x491F0)
 padding(0x491F0, 0x49800, b"U")
 
 # Typing data
-at(0x49800, b"00\0add\0af\0as")
+check(0x49800, b"00\0add\0af\0as")
 
-padding(0x49be8, 0x49cf0, b"\0")
-padding(0x49cf0, 0x4a000, b"U")
-unknown_data = rom[0x4a000:0x4a594]
-padding(0x4a594, 0x4b780, b"\xe5")
-padding(0x4a594, 0x4b780, b"\xe5")
-unknown_data2 = rom[0x4b780:0x4bc00]
-strings("item", 0x4bc00, 0x4bff0)
-strings("glob", 0x4c000, 0x4cbf0)
-padding(0x4cbf0, 0x4d000, b"U")
-strings("comm", 0x4d000, 0x4dff0)
-padding(0x4e000, 0x50000, b"\xe5")
+padding(0x49BE8, 0x49CF0, b"\0")
+padding(0x49CF0, 0x4A000, b"U")
+unknown_data = rom[0x4A000:0x4A594]
+padding(0x4A594, 0x4B780, b"\xe5")
+padding(0x4A594, 0x4B780, b"\xe5")
+unknown_data2 = rom[0x4B780:0x4BC00]
+strings("item", 0x4BC00, 0x4BFF0)
+strings("glob", 0x4C000, 0x4CBF0)
+padding(0x4CBF0, 0x4D000, b"U")
+strings("comm", 0x4D000, 0x4DFF0)
+padding(0x4E000, 0x50000, b"\xe5")
 
-at(0x50000, b"MAP TOWN01/02")
+check(0x50000, b"MAP TOWN01/02")
 map_town12 = rom[0x50000:0x54000]
 
-strings("twn1", 0x54000, 0x555f0)
-strings("twn2", 0x56000, 0x574f0)
+strings("twn1", 0x54000, 0x555F0)
+strings("twn2", 0x56000, 0x574F0)
 
-at(0x58000, b"TWN03 MAP")
-map_town3 = rom[0x58000:0x5c000]
-strings("twn3", 0x5c000, 0x5d6f0)
-strings("twn4", 0x5e000, 0x5faf0)
+check(0x58000, b"TWN03 MAP")
+map_town3 = rom[0x58000:0x5C000]
+strings("twn3", 0x5C000, 0x5D6F0)
+strings("twn4", 0x5E000, 0x5FAF0)
 
-at(0x60000, b"MAP:TWN04/06")
-strings("twn5", 0x64000, 0x656f0)
-strings("twn6", 0x66000, 0x66af0)
-at(0x68000, b"MAP TOWN 07")
+check(0x60000, b"MAP:TWN04/06")
+strings("twn5", 0x64000, 0x656F0)
+strings("twn6", 0x66000, 0x66AF0)
+check(0x68000, b"MAP TOWN 07")
 
-
-
-def onebpp(bs, width=64, height=64):
-    rows = []
-    for y in range(height):
-        row = []
-        for x in range(width // 8):
-            for b in range(8):
-                bit = bs[width//8*y+x] >> (7-b) & 1
-                row.append(bit)
-        rows.append(row)
-
-    return rows
-
-
-def twobpp(bs, width=64, height=64):
-    l1 = onebpp(bs, width, height)
-    l2 = onebpp(bs[width//8 * height:], width, height)
-    return [[r+2*g for r,g in zip(rr,gg)] for rr,gg in zip(l1,l2)]
-
-def threebpp(bs, width=64, height=64):
-    l1 = onebpp(bs, width, height)
-    l2 = onebpp(bs[width//8 * height:], width, height)
-    l3 = onebpp(bs[width//8 * height * 2:], width, height)
-    return [[r+2*g+4*b for r,g,b in zip(rr,gg,bb)] for rr,gg,bb in zip(l1,l2,l3)]
-
-def show(matrix):
-    for row in matrix:
-        print("".join("\x1b[%dm%s\x1b[0m" % ([40, 102,107,103,101,106,105,104][x],"  ") for x in row))
-
-if False: # I was looking for sprites
-    start = 0x10000
-    w = 64
-    h = 64
-    for i in range(2000):
-        addr = start + i * w*h//8
-        print(f"0x{addr:05x}")
-        show(onebpp(rom[addr:], w, h))
-        input()
 
 def write(addr, text):
     if isinstance(text, str):
         text = text.encode("shift-jis")
-    patched[addr:addr+len(text)] = text
+    patched[addr : addr + len(text)] = text
+
 
 # 0xc0000:0xcd800 = monster data
-ptr = 0xc0000
+ptr = 0xC0000
 
 monsters = [
     "Regular Slime",
@@ -203,38 +173,27 @@ monsters = [
 assert len(monsters) == 27
 
 for i, name in enumerate(monsters):
-    #print(i, bytes(rom[ptr+2:ptr+16]).rstrip(b"\0").decode("ascii"), bytes(rom[ptr+0x70:ptr+0x7e]).rstrip(b"\0").decode("shift-jis"))
     write(ptr + 0x70, name + "\0")
-    red   = onebpp(bytes(rom[ptr+0x200:ptr+0x400]))
-    green = onebpp(bytes(rom[ptr+0x400:ptr+0x600]))
-    blue  = onebpp(bytes(rom[ptr+0x600:ptr+0x800]))
-    if False:
-        total = [[1*r+2*g+4*b for r,g,b in zip(rr,gg,bb)] for rr,gg,bb in zip(red,green,blue)]
-        for row in total: print("".join("\x1b[%dm%s\x1b[0m" % ([40, 102,107,103,101,106,105,104][x],"  ") for x in row))
-
-
     ptr += 0x800
 
-assert ptr == 0xcd800
-padding(0xcd800, 0xd0000, b"\xe5")
+assert ptr == 0xCD800
+padding(0xCD800, 0xD0000, b"\xe5")
 
-# bosses???
-ptr = 0xd0000
+ptr = 0xD0000
 bosses = "Hidama Bou Senju Kasumi Uzumaneki Modoki Oonetsu Ishime Ryuu".split()
 assert len(bosses) == 9
 for i, name in enumerate(bosses):
-    # print(i, bytes(rom[ptr+2:ptr+14]).rstrip(b"\0").decode("ascii"), bytes(rom[ptr+0x70:ptr+0x7e]).rstrip(b"\0").decode("shift-jis"))
     write(ptr + 0x70, name + "\0")
     ptr += 0x1000
 
-assert ptr == 0xd9000
-padding(0xd8f80, 0x134000, b"\xe5")
+assert ptr == 0xD9000
+padding(0xD8F80, 0x134000, b"\xe5")
 assert len(rom) == 0x134000
 
-write(0x13270, "Talk/Look\rItems\rSpell\rKeyboard\rScore\rSystem\0")
+write(0x13270, "Talk/Look\rInventory\rSpell\rKeyboard\rScore\rSystem\0")
 
 # Patch out the copy protection check in the final dungeon.
-at(0xB85AE, b"\x75\x03")     # jnz $+5
+check(0xB85AE, b"\x75\x03")  # jnz $+5
 write(0xB85AE, b"\x90\x90")  # nop / nop
 
 # Write a ROM with inserted text:
