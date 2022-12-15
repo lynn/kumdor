1bbcd8b48ea32c6dbbf6e21159d49ee9bdc5820b
Lynn <lynn@foldr.moe>
2022-12-15 13:37:25 +0100

    Some twn4

--- text/twn4.py
+++ text/twn4.py
@@ -3,22 +3,22 @@
     "00",
 
     # 01 = "まいどー。全部で "
-    "まいどー。全部で ",
+    "That'll be ",
 
     # 02 = "スパイスですが。"
-    "スパイスですが。",
+    " spice, please.",
 
     # 03 = "???"
     "???",
 
     # 04 = "ちょっとスパイスが足らないようですが…。"
-    "ちょっとスパイスが足らないようですが…。",
+    "You're a little short on money...",
 
     # 05 = "残念ながら もう持ち切れませんね。"
-    "残念ながら もう持ち切れませんね。",
+    "Oh, you're carrying too much.",
 
     # 06 = "ありがとうございます。又どうぞ。"
-    "ありがとうございます。又どうぞ。",
+    "Thank you, come again.",
 
     # 07 = "07"
     "07",
@@ -51,181 +51,181 @@
     "10",
 
     # 11 = "クムドールの首都 クミエルにようこそ。"
-    "クムドールの首都 クミエルにようこそ。",
+    "~Welcome to Kumiel, capital of Kumdor.",
 
     # 12 = "キーショップでござい。"
-    "キーショップでござい。",
+    "~Welcome to our key shop.",
 
     # 13 = "キーはいかがでしょう。"
-    "キーはいかがでしょう。",
+    "~Please browse our collection.",
 
     # 14 = "キー、買ってください。"
     "キー、買ってください。",
 
     # 15 = "キーは全部揃いましたか。"
-    "キーは全部揃いましたか。",
+    "~Do you have a full set of keys?",
 
     # 16 = "ホテル クミエルにようこそ。"
-    "ホテル クミエルにようこそ。",
+    "Welcome to Hotel Kumiel.",
 
     # 17 = "お泊まりでございましょうか。\u0015一泊 40スパイスです。"
-    "お泊まりでございましょうか。\u0015一泊 40スパイスです。",
+    "~Can I offer you a one-person room at 40spc/night?",
 
     # 18 = "ありがとうございます。\u0015では "
-    "ありがとうございます。\u0015では ",
+    "Thank you. Please use room #",
 
     # 19 = "号室をお使いください。"
-    "号室をお使いください。",
+    ".",
 
     # 1a = "あ おはようございます。"
-    "あ おはようございます。",
+    "Ah, good morning.",
 
     # 1b = "1B"
-    "1B",
+    "",
 
     # 1c = "1C"
-    "1C",
+    "",
 
     # 1d = "1D"
-    "1D",
+    "",
 
     # 1e = "1E"
-    "1E",
+    "",
 
     # 1f = "1F"
-    "1F",
+    "",
 
     # 20 = "20"
-    "20",
+    "",
 
     # 21 = "1 2 3と! \" #キーについて…"
-    "1 2 3と! \" #キーについて…",
+    "About the 123 and !\"# keys...",
 
     # 22 = "1は、"
-    "1は、",
+    "To type 1, ",
 
     # 23 = "Aに置いてある左手の小指を使って打つ。\u0015"
-    "Aに置いてある左手の小指を使って打つ。\u0015",
+    "use the left pinky (on A).\u0015",
 
     # 24 = "（1を打て）"
-    "（1を打て）",
+    "(press 1)",
 
     # 25 = "2は、"
-    "2は、",
+    "To type 2, ",
 
     # 26 = "Sに置いてある左手の薬指を使って打つ。\u0015"
-    "Sに置いてある左手の薬指を使って打つ。\u0015",
+    "use your ring finger (on S).\u0015",
 
-    # 27 = "（2を打て）"
-    "（2を打て）",
+    # 27 = "(press 2)"
+    "(press 2)",
 
     # 28 = "3は、"
-    "3は、",
+    "To type 3, ",
 
     # 29 = "Dに置いてある左手の中指を使って打つ。\u0015"
-    "Dに置いてある左手の中指を使って打つ。\u0015",
+    "use your middle finger (on D).\u0015",
 
-    # 2a = "（3を打て）"
-    "（3を打て）",
+    # 2a = "(press 3)"
+    "(press 3)",
 
     # 2b = "!は、"
-    "!は、",
+    "To type !, ",
 
     # 2c = "右手の小指でSHIFTキーを押しながら\u0015"
-    "右手の小指でSHIFTキーを押しながら\u0015",
+    "hold Shift with your right pinky and\u0015",
 
     # 2d = "（!を打て）"
-    "（!を打て）",
+    "(press !)",
 
     # 2e = "\"は、"
-    "\"は、",
+    "To type \", ",
 
     # 2f = "（\"を打て）"
-    "（\"を打て）",
+    "(press \")",
 
     # 30 = "30"
     "30",
 
     # 31 = "#は、"
-    "#は、",
+    "To type #, ",
 
     # 32 = "（#を打て）"
-    "（#を打て）",
+    "(press #)",
 
     # 33 = "8 9 0と( )キーについて…"
-    "8 9 0と( )キーについて…",
+    "About the 890 and () keys...",
 
     # 34 = "8は、"
-    "8は、",
+    "To type 8, ",
 
     # 35 = "Kに置いてある右手の中指を使って打つ。\u0015"
-    "Kに置いてある右手の中指を使って打つ。\u0015",
+    "use your right middle finger.\u0015",
 
     # 36 = "（8を打て）"
-    "（8を打て）",
+    "(press 8)",
 
     # 37 = "9は、"
-    "9は、",
+    "To type 9, ",
 
     # 38 = "Lに置いてある右手の薬指を使って打つ。\u0015"
-    "Lに置いてある右手の薬指を使って打つ。\u0015",
+    "use your right ring finger.\u0015",
 
     # 39 = "（9を打て）"
-    "（9を打て）",
+    "(press 9)",
 
     # 3a = "0は、"
-    "0は、",
+    "To type 0, ",
 
     # 3b = ";に置いてある右手の小指を使って打つ。\u0015"
-    ";に置いてある右手の小指を使って打つ。\u0015",
+    "use your right pinky.\u0015",
 
     # 3c = "（0を打て）"
     "（0を打て）",
 
     # 3d = "(は、"
-    "(は、",
+    "To type (, ",
 
     # 3e = "左手の小指でSHIFTキーを押しながら\u0015"
-    "左手の小指でSHIFTキーを押しながら\u0015",
+    "hold Shift with the left pinky and\u0015",
 
     # 3f = "（(を打て）"
-    "（(を打て）",
+    "(press ()",
 
     # 40 = "40"
     "40",
 
     # 41 = ")は、"
-    ")は、",
+    "To type ),",
 
     # 42 = "（)を打て）"
-    "（)を打て）",
+    "(press ))",
 
     # 43 = "以上。"
-    "以上。",
+    "That is all.",
 
     # 44 = "タマオリンを使えば、石化の原因も\u0015判るかも知れないな。"
-    "タマオリンを使えば、石化の原因も\u0015判るかも知れないな。",
+    "~Using Tamaorin, we may even discover the cause of the petrification.",
 
     # 45 = "この研究所も、長年クムの木を調べてきたが\u0015当の本人に聞く−\u0015という方法は考えつかなかったな。"
-    "この研究所も、長年クムの木を調べてきたが\u0015当の本人に聞く−\u0015という方法は考えつかなかったな。",
+    "~This facility has been researching keytrees for years, but we never thought to simply ask them stuff.",
 
     # 46 = "タマオ博士が? ウスカ記念公園で研究を?\u0011\u0015なる程、あそこのクムの木は\u0015イキがいいからな。"
-    "タマオ博士が? ウスカ記念公園で研究を?\u0011\u0015なる程、あそこのクムの木は\u0015イキがいいからな。",
+    "~Dr. Tamao is doing research at Usuka Memorial Park? That makes sense...\x15The keytrees are in good shape there.",
 
     # 47 = "え タマオ博士が−\u0015クムの木と話せる薬を完成したんですか?\u0011\u0015\u0017（メモを渡した。）"
-    "え タマオ博士が−\u0015クムの木と話せる薬を完成したんですか?\u0011\u0015\u0017（メモを渡した。）",
+    "~Huh? Dr. Tamao developed a medicine that lets you talk to keytrees!?\x11\x15\x17(Handed over the notes.)",
 
     # 48 = "これを調合すればいいんですね…。\u0011\u0015ちょうど今、一個分の材料がありますから\u0015さっそく試作してみましょう。"
-    "これを調合すればいいんですね…。\u0011\u0015ちょうど今、一個分の材料がありますから\u0015さっそく試作してみましょう。",
+    "~We should synthesize this recipe...\u0011\u0015We have everything on hand for one vial,\u0015so I'll make a trial dose right away.",
 
     # 49 = "これでいいのかな…。\u0011\u0015出来ました、メモの配合通りです。薬の名前−\u0015「タマオリン」と言うのはどうですか。"
-    "これでいいのかな…。\u0011\u0015出来ました、メモの配合通りです。薬の名前−\u0015「タマオリン」と言うのはどうですか。",
+    "~This should do the trick...\u0011\u0015There, just what the doctor ordered.\u0015I call it... \"Tamaorin\"!",
 
     # 4a = "これは提供しますが、材料が高価なので\u0015次からは実費で 500スパイス程かかりますよ。\u0011\u0015\u0017（タマオリンを受け取った。）"
-    "これは提供しますが、材料が高価なので\u0015次からは実費で 500スパイス程かかりますよ。\u0011\u0015\u0017（タマオリンを受け取った。）",
+    "You can have this one for free, but the\u0015subsequent doses will be 500 spice.\u0011\u0015\u0017(Obtained Tamaorin.)",
 
     # 4b = "今、材料を集めさせてますから…。"
-    "今、材料を集めさせてますから…。",
+    "I'm having the ingredients gathered...",
 
     # 4c = "4C"
     "4C",
@@ -234,139 +234,139 @@
     "4D",
 
     # 4e = "500スパイスかかりますが−\u0015タマオリンをもっと作りますか?"
-    "500スパイスかかりますが−\u0015タマオリンをもっと作りますか?",
+    "~Shall I make more Tamaorin? This time it'll cost 500 spice...",
 
     # 4f = "では二、三日待ってください。"
-    "では二、三日待ってください。",
+    "~Okay. Please wait 2-3 days.",
 
     # 50 = "50"
     "50",
 
     # 51 = "例のタマオリンですが、いい話があります。\u0011\u0015チリー通商の社長という人に話したら、\u0015売れそうだから、扱いたいと言うんですよ。"
-    "例のタマオリンですが、いい話があります。\u0011\u0015チリー通商の社長という人に話したら、\u0015売れそうだから、扱いたいと言うんですよ。",
+    "I have good news about Tamaorin!\u0011\u0015I talked to a pharmaceutical company,\u0015and they'd like to mass-produce it.",
 
     # 52 = "大量生産するから、ずっと安くなるそうです。\u0015二、三日したらマーケットに\u0015出荷出来ると言ってました。"
-    "大量生産するから、ずっと安くなるそうです。\u0015二、三日したらマーケットに\u0015出荷出来ると言ってました。",
+    "That means it'll become a lot cheaper.\u0015You'll find it at the local market\u0015starting 2-3 days from now.",
 
     # 53 = "\u0017（その他大勢のようだ。）"
-    "\u0017（その他大勢のようだ。）",
+    "~\u0017(He looks like your average man in the street.)",
 
     # 54 = "私、その他大勢です。"
-    "私、その他大勢です。",
+    "I'm just your average man in the street.",
 
     # 55 = "えーっと、どなたでしたっけ?"
-    "えーっと、どなたでしたっけ?",
+    "Ah, who are you again?",
 
     # 56 = "ども、コンチワ。"
-    "ども、コンチワ。",
+    "G'day.",
 
     # 57 = "このマンションは、\u0015その他大勢の人しかいないよ。"
-    "このマンションは、\u0015その他大勢の人しかいないよ。",
+    "~Everyone in this condo is just your average man in the street.",
 
     # 58 = "この町は大きいからね、\u0015その他大勢の人に話しかけても\u0015時間のムダかもね。"
-    "この町は大きいからね、\u0015その他大勢の人に話しかけても\u0015時間のムダかもね。",
+    "~This is the big city. Talking to your average man in the street might be a waste of time.",
 
     # 59 = "ここは 203号室ですけど…。"
-    "ここは 203号室ですけど…。",
+    "~Sorry, this is room #203...",
 
     # 5a = "村ごと湖に沈んじゃってね、\u0015やっと空港まで逃げ延びたと思ったら−\u0015今度は噴火だと。ツイてないよまったく。"
-    "村ごと湖に沈んじゃってね、\u0015やっと空港まで逃げ延びたと思ったら−\u0015今度は噴火だと。ツイてないよまったく。",
+    "~First, my whole village sinks into a lake. Then I evacuate here, and a volcano erupts! I can't get a break.",
 
     # 5b = "輸出用の荷物を積み終わったところで\u0015あの噴火だろ。\u0015一文無しだ。"
-    "輸出用の荷物を積み終わったところで\u0015あの噴火だろ。\u0015一文無しだ。",
+    "~We'd just finished loading the cargo for export when that volcano erupted... Financially, we're doomed.",
 
     # 5c = "あ、おかげでタマオリン大当り。\u0015感謝してます。"
-    "あ、おかげでタマオリン大当り。\u0015感謝してます。",
+    "~Thanks to you, I'm rolling in Tamaorin money. You're a life-saver!",
 
     # 5d = "ここにいても…\u0015もう、給料は貰えそうもないな。"
-    "ここにいても…\u0015もう、給料は貰えそうもないな。",
+    "~Why stay here? I probably won't get a paycheck.",
 
     # 5e = "ニコ ニコ。"
-    "ニコ ニコ。",
+    "*grin*",
 
     # 5f = "家なんて、無理して買う事ないぜ。\u0015金が余った時でいいんじゃない。"
-    "家なんて、無理して買う事ないぜ。\u0015金が余った時でいいんじゃない。",
+    "~Don't buy a house when money is tight. Save up a little first.",
 
     # 60 = "60"
-    "60",
+    "",
 
     # 61 = "おかしな夢を見たもんだ。\u0015どんな夢かって?\u0015まあ、秘密にする程の内容じゃないけど…"
-    "おかしな夢を見たもんだ。\u0015どんな夢かって?\u0015まあ、秘密にする程の内容じゃないけど…",
+    "~I had a strange dream. What was it, you ask? Well, I'll divulge it to you...",
 
     # 62 = "火の海の中に空港があってね…\u0015建物の中に入って行くとロボットがいて\u0015そのうしろに見えない階段があるんだが−"
-    "火の海の中に空港があってね…\u0015建物の中に入って行くとロボットがいて\u0015そのうしろに見えない階段があるんだが−",
+    "I saw an airport in a sea of flames...\x15I entered a building, and there was an\x15invisible staircase behind a robot.",
 
     # 63 = "「お前には風見タヌキは必要ないから帰れ」\u0015って言うんだ。  それだけなんだけど…\u0015「風見タヌキ」って何だろ。"
-    "「お前には風見タヌキは必要ないから帰れ」\u0015って言うんだ。  それだけなんだけど…\u0015「風見タヌキ」って何だろ。",
+    "~It said, \"you don't deserve this whithervane, so buzz off!\" What even is a \"whithervane\"?",
 
     # 64 = "あの断層は飛び越えられないよ。\u0015地下から強い熱風が吹いてきて−\u0015押し戻されちゃうんだ。"
-    "あの断層は飛び越えられないよ。\u0015地下から強い熱風が吹いてきて−\u0015押し戻されちゃうんだ。",
+    "~There's no crossing those chasms. Strong wind blows up from them, knocking you right back.",
 
     # 65 = "宮殿は、このクミエルの北にあるんだが\u0015断層が出来て 行けなくなってしまったよ。"
-    "宮殿は、このクミエルの北にあるんだが\u0015断層が出来て 行けなくなってしまったよ。",
+    "~The palace is north from here, but you can't get there with those chasms in the way.",
 
     # 66 = "クムドール空港はちょうどこの南にあるんだが\u0015今頃は溶岩の中だろう。"
-    "クムドール空港はちょうどこの南にあるんだが\u0015今頃は溶岩の中だろう。",
+    "~Kumdor Airport is just south from here, but it's submerged in lava now.",
 
     # 67 = "空港にいた人達は−\u0015全員このクミエルに逃げて来たんだよ。\u0015いきなり火山が噴火してね。"
-    "空港にいた人達は−\u0015全員このクミエルに逃げて来たんだよ。\u0015いきなり火山が噴火してね。",
+    "~When the volcano erupted, everyone at the airport evacuated here to Kumiel.",
 
     # 68 = "宮殿には、この大異変の対策本部があるんだが\u0015これだけメチャクチャになると−\u0015手も足も出ないだろうなあ。"
-    "宮殿には、この大異変の対策本部があるんだが\u0015これだけメチャクチャになると−\u0015手も足も出ないだろうなあ。",
+    "The palace has established a task force\x15to deal with the disaster, but I doubt\x15they'll be able to solve anything.",
 
     # 69 = "変な手のおかげで、\u0015納税の書類が全部無くなっちゃった。\u0015どーしたらいいんだろ。"
-    "変な手のおかげで、\u0015納税の書類が全部無くなっちゃった。\u0015どーしたらいいんだろ。",
+    "~Because of those creepy hands, everyone's tax forms have gone missing. What should we do...",
 
     # 6a = "ここは税務署です。\u0015納税に来られたんでしょう?\u0015え この国の人じゃないの。"
-    "ここは税務署です。\u0015納税に来られたんでしょう?\u0015え この国の人じゃないの。",
+    "~This is the tax office. Are you here to file a report? Wait, you're not from around here.",
 
     # 6b = "救国タイピング教室にようこそ。"
-    "救国タイピング教室にようこそ。",
+    "~Welcome to the National Defense Typing Class.",
 
     # 6c = "1分間に 6ワード!?\u0015どうしてクムドール人って\u0015こうも不器用なんだろ。"
-    "1分間に 6ワード!?\u0015どうしてクムドール人って\u0015こうも不器用なんだろ。",
+    "~Six words per minute!? Maybe our people just aren't cut out for this.",
 
     # 6d = "これじゃ、呪文の壁を破るどころか\u0015魔獣とも戦えないよ。"
-    "これじゃ、呪文の壁を破るどころか\u0015魔獣とも戦えないよ。",
+    "~I'm no match for the monsters at this rate--let alone the spell walls.",
 
     # 6e = "自信のある人は、クム研の２階に\u0015きも試しに行くんだが−\u0015なかなか難しくってね。"
-    "自信のある人は、クム研の２階に\u0015きも試しに行くんだが−\u0015なかなか難しくってね。",
+    "~Those who dare can go test their skills on the 2nd floor of Key U. But it won't be easy...",
 
     # 6f = "えーと Eは左手の親指じゃなくって\u0015…どの指で打つんだっけ。"
-    "えーと Eは左手の親指じゃなくって\u0015…どの指で打つんだっけ。",
+    "~Which finger is used to type E again... the thumb?",
 
     # 70 = "70"
-    "70",
+    "",
 
     # 71 = "ノニトト モイ コチコン!\u0015いけない、カナキーがロックされてた。"
-    "ノニトト モイ コチコン!\u0015いけない、カナキーがロックされてた。",
+    "wELCOME TO kUMDOR!\u0015Oops, I had Caps Lock on.",
 
     # 72 = "上の階では有志が集まって訓練しているが−\u0015当分の間クムドールは\u0015救われそうもない感じだね。"
-    "上の階では有志が集まって訓練しているが−\u0015当分の間クムドールは\u0015救われそうもない感じだね。",
+    "~Volunteers are taking typing lessons upstairs, but well... things don't look too good for Kumdor.",
 
     # 73 = "マーケットの裏手にある家は−\u0015地下から変な音が聞こえてくるんで\u0015買い手がつかないという話だよ。"
-    "マーケットの裏手にある家は−\u0015地下から変な音が聞こえてくるんで\u0015買い手がつかないという話だよ。",
+    "~I hear nobody wants to buy the house behind the market because of the strange noises coming from the basement.",
 
     # 74 = "数字がうまく打てないなあ。"
-    "数字がうまく打てないなあ。",
+    "I always mess up the digits.",
 
     # 75 = "えー 大文字は小指でシフトキーを押しながら\u0015…だめですよ離しちゃ。\u0015押しながら打つんですよ。"
-    "えー 大文字は小指でシフトキーを押しながら\u0015…だめですよ離しちゃ。\u0015押しながら打つんですよ。",
+    "So, for capital letters, hold Shift--\x15No, see, don't let go, *hold* it down,\u0015then type the letter...",
 
     # 76 = "地球から"
-    "地球から",
+    "If only ",
 
     # 77 = "様が来てくれれば\u0015こんな目にあわずに済んだのになあ。"
-    "様が来てくれれば\u0015こんな目にあわずに済んだのになあ。",
+    " had come from Earth,\x15we wouldn't be in this mess.",
 
     # 78 = "「風見タヌキ」があると−\u0015断層を越えられるんだよ。\u0015あ これは夢の中の話だったっけ。"
-    "「風見タヌキ」があると−\u0015断層を越えられるんだよ。\u0015あ これは夢の中の話だったっけ。",
+    "~If you have a \"whithervane\", you can cross the chasms. Well, that's how it worked in my dream.",
 
     # 79 = "南東の壁に挑戦しようとして−\u0015123と890のキーを買っていくらしいんだが、\u0015-^\\と[:]_キーは一体どこで使うんだろう。"
-    "南東の壁に挑戦しようとして−\u0015123と890のキーを買っていくらしいんだが、\u0015-^\\と[:]_キーは一体どこで使うんだろう。",
+    "~You'll want the 123 and 890 keys to face the spell wall to the east. But who even uses the -^\\ and [:]_ keys?",
 
     # 7a = "ここは王立クムの木研究所−\u0015略して「クム研」です。"
-    "ここは王立クムの木研究所−\u0015略して「クム研」です。",
+    "~This is the Royal Institute for Keytree Research... known as \"Key U\" for short.",
 
     # 7b = "きも試しに来たんですけど…\u0015え 税務署の２階を済ましてから?"
     "きも試しに来たんですけど…\u0015え 税務署の２階を済ましてから?",
