252fae2fcaca59b2a10d4c2a490e91d53aa0343c
Lynn <lynn@foldr.moe>
2023-01-10 16:18:31 +0100

    twn5 TL

--- text/twn5.py
+++ text/twn5.py
@@ -57,10 +57,10 @@
     "We know our messenger reached them,\u0015but then the aircraft stopped working...\u0015I don't think they'll make it here.",
 
     # 13 = "え あなたが"
-    "Wait, *you're* ",
+    "You say *you're* ",
 
     # 14 = "ですって?\u0011\u0015それが本当なら きっと…\u0015クムドールは救われる事でしょうね。"
-    "?\u0011\u0015If that's true, then Kumdor is saved...!",
+    "?\u0011\u0015If that were so, then Kumdor wouldn't\x15still be in this mess, would we?",
 
     # 15 = "私が本物の"
     "I'm the real ",
@@ -78,350 +78,350 @@
     " is a guy?",
 
     # 1a = "異変対策本部という言い方だが…\u0015大異変対策本部に変更すべきじゃないだろか。"
-    "異変対策本部という言い方だが…\u0015大異変対策本部に変更すべきじゃないだろか。",
+    "We're called the \"Anomaly Task Force\",\x15but... I propose a rebranding to\x15\"Anomaly Action Force.\"",
 
     # 1b = "あわてて戦うより、百姓でもやりながら\u0015少し時期を見た方がいいと思うんだが。"
-    "あわてて戦うより、百姓でもやりながら\u0015少し時期を見た方がいいと思うんだが。",
+    "~Rather than rushing in fighting, I think we should monitor the situation and focus on agriculture for now.",
 
     # 1c = "ブラインドタッチ猛特訓という案だが−\u0015一日一時間位におさえないと\u0015指がひきつっちゃうんじゃないか?"
-    "ブラインドタッチ猛特訓という案だが−\u0015一日一時間位におさえないと\u0015指がひきつっちゃうんじゃないか?",
+    "~Our citizens can only practice touch-typing for about an hour a day before their fingers cramp up.",
 
     # 1d = "もう、あなたが来てくれたんだから−\u0015会議をやるだけ時間のムダかも。"
-    "もう、あなたが来てくれたんだから−\u0015会議をやるだけ時間のムダかも。",
+    "~Now that you're here, this meeting might be a waste of time.",
 
     # 1e = "毎日、会議ばっかやってたもんで\u0015戦うエネルギーが無くなってきた。"
-    "毎日、会議ばっかやってたもんで\u0015戦うエネルギーが無くなってきた。",
+    "~We have so many meetings every day, I've lost the energy to fight.",
 
     # 1f = "グー グー。"
-    "グー グー。",
+    "zzz...",
 
     # 20 = "20"
-    "20",
+    "",
 
     # 21 = "ハイテクが駄目でテレビが見れないから−\u0015風力テレビと言うのを開発したらどうだろう。"
-    "ハイテクが駄目でテレビが見れないから−\u0015風力テレビと言うのを開発したらどうだろう。",
+    "~We must address the technology outage. What about a wind-powered TV?",
 
     # 22 = "案としては、327ほど出たんだが\u0015使えそうな案は一つもありません。\u0011\u0015いいえー、もちろん恥じてますよ。"
-    "案としては、327ほど出たんだが\u0015使えそうな案は一つもありません。\u0011\u0015いいえー、もちろん恥じてますよ。",
+    "~We've come up with 327 suggestions, but not a single one has proven useful. It's quite embarrassing.",
 
     # 23 = "それにしても、クムドールの人達は\u0015頼りにならないわね。\u0015もう全面的にあなたにお願いするしかないわ。"
-    "それにしても、クムドールの人達は\u0015頼りにならないわね。\u0015もう全面的にあなたにお願いするしかないわ。",
+    "~We just can't rely on our people. We have no choice but to put it all in your hands.",
 
     # 24 = "あーっ この間夢の中で会った人ね。\u0011\u0015確かあの時、あなたは「今度宮殿に行くから\u0015裏のドアを開けて。」って言ってたわね。"
-    "あーっ この間夢の中で会った人ね。\u0011\u0015確かあの時、あなたは「今度宮殿に行くから\u0015裏のドアを開けて。」って言ってたわね。",
+    "~Oh, I saw you in my dream! You told me to keep the door open for you to enter the palace.",
 
     # 25 = "信じられない事だけど…\u0015約束だから 開けてあげる。"
-    "信じられない事だけど…\u0015約束だから 開けてあげる。",
+    "I know it's hard to believe, but...\x15I promised I'd let you in, so go ahead.",
 
     # 26 = "今夜はどんな夢を見るかしら。"
-    "今夜はどんな夢を見るかしら。",
+    "What will I dream next...",
 
     # 27 = "私は国王だが… \u0011あんた誰?"
-    "私は国王だが… \u0011あんた誰?",
+    "I am the king. Who are you?",
 
     # 28 = "えええええええええええええーっ!!!\u0015本当ですか? そりゃ大変。"
-    "えええええええええええええーっ!!!\u0015本当ですか? そりゃ大変。",
+    "Whaaaaaaaat!?\u0015You're serious? Oh my stars.",
 
     # 29 = "今 隣で会議の最中なんだが−\u0015ぜひ顔を出してやってください。\u0011\u0015おーい! ドアを開けてあげなさい。"
-    "今 隣で会議の最中なんだが−\u0015ぜひ顔を出してやってください。\u0011\u0015おーい! ドアを開けてあげなさい。",
+    "People are holding a meeting next door.\x15Please go tell them you're here.\x15\x11Hey! Open the door for this Earthling!",
 
     # 2a = "うーむ。\u0015この本は なかなか面白い。"
-    "うーむ。\u0015この本は なかなか面白い。",
+    "This book is quite interesting.",
 
     # 2b = "女王陛下はただ今 重要会議中ですので−\u0015お引き取りください。"
-    "女王陛下はただ今 重要会議中ですので−\u0015お引き取りください。",
+    "~Her Majesty is in an important meeting right now, so please leave.",
 
     # 2c = "開いてますよ。どうぞ。"
-    "開いてますよ。どうぞ。",
+    "It's open. Go ahead.",
 
     # 2d = "ダメですよ。勝手に入って来ちゃ。\u0011\u0015えっ "
-    "ダメですよ。勝手に入って来ちゃ。\u0011\u0015えっ ",
+    "Hey, you can't just waltz into...\u0011\u0015What? ",
 
     # 2e = "様!?\u0015もうこちらには来られないと思ってました。"
-    "様!?\u0015もうこちらには来られないと思ってました。",
+    "?!\u0015We thought you'd never make it here.",
 
     # 2f = "皆さん!! グッドニュースですよ。\u0015"
-    "皆さん!! グッドニュースですよ。\u0015",
+    "Listen up, everyone! Excellent news.\u0015",
 
     # 30 = "30"
-    "30",
+    "",
 
     # 31 = "様が来てくれましたよー。"
-    "様が来てくれましたよー。",
+    " made their way here.",
 
     # 32 = "あ "
-    "あ ",
+    "Ah, ",
 
     # 33 = "様。"
-    "様。",
+    ".",
 
     # 34 = "様ですよね。\u0011\u0015ええ、よく存じておりますとも。\u0015ど どうぞ、お入りください。"
-    "様ですよね。\u0011\u0015ええ、よく存じておりますとも。\u0015ど どうぞ、お入りください。",
+    ", right?\u0011\u0015W-why of course! You're a celebrity.\x15P-please, come right in.",
 
     # 35 = "あんた、本物だったの。"
-    "あんた、本物だったの。",
+    "So you really are the one and only.",
 
     # 36 = "女王様は… えーと\u0011\u0015下で会議中じゃないですか?"
-    "女王様は… えーと\u0011\u0015下で会議中じゃないですか?",
+    "The queen is in a meeting downstairs.",
 
     # 37 = "王様ったら、図書室で\u0015寝起きしてるんですものね。"
-    "王様ったら、図書室で\u0015寝起きしてるんですものね。",
+    "~That king, always sleeping in the library!",
 
     # 38 = "38"
-    "38",
+    "",
 
     # 39 = "何も手助け出来なくてすみません。\u0011\u0015そうだわ、もしよければ隣のお部屋を\u0015使ってください。"
-    "何も手助け出来なくてすみません。\u0011\u0015そうだわ、もしよければ隣のお部屋を\u0015使ってください。",
+    "Sorry we weren't of any help.\x11\x15Ah, feel free to use the room next door.",
 
     # 3a = "3A"
-    "3A",
+    "",
 
     # 3b = "みんな、やる事が無くなったもんで−\u0015外に出て野良仕事にはげんでるようですよ。"
-    "みんな、やる事が無くなったもんで−\u0015外に出て野良仕事にはげんでるようですよ。",
+    "~Nobody has anything left to do, so they've gone to work in the fields.",
 
     # 3c = "ここはウスカ記念公園。\u0011\u0015以前は姫様がミドとかいう名の少年と一緒に\u0015よく散歩に来られたんだが…。"
-    "ここはウスカ記念公園。\u0011\u0015以前は姫様がミドとかいう名の少年と一緒に\u0015よく散歩に来られたんだが…。",
+    "~This is Usuka Memorial Park. The princess used to visit here together with a boy named Mido...",
 
     # 3d = "びしょ濡れじゃないか!\u0015水遊びしてたのかい?\u0011\u0015"
-    "びしょ濡れじゃないか!\u0015水遊びしてたのかい?\u0011\u0015",
+    "~You're soaking wet! Were you playing in the water?",
 
     # 3e = "見学に来たのなら謝るよ。このウスカ記念館は\u0015私が研究室がわりに使ってるんでね…。\u0011\u0015"
-    "見学に来たのなら謝るよ。このウスカ記念館は\u0015私が研究室がわりに使ってるんでね…。\u0011\u0015",
+    "~If you're here for a tour, I'm sorry.\x15I've turned this Usuka Memorial building\x15into my lab...\u0011\u0015",
 
     # 3f = "それにしても、どうやってここに来たんだい?\u0015道は全部ふさがってるはずだが…。"
-    "それにしても、どうやってここに来たんだい?\u0015道は全部ふさがってるはずだが…。",
+    "For that matter, how did you get here?\x15All the roads are blocked off.",
 
     # 40 = "40"
-    "40",
+    "",
 
     # 41 = ";に置いてある右手の小指を使って打つ。\u0015"
-    ";に置いてある右手の小指を使って打つ。\u0015",
+    "use the right pinky.\u0015",
 
     # 42 = "左手の小指でSHIFTキーを押しながら\u0015"
-    "左手の小指でSHIFTキーを押しながら\u0015",
+    "hold Shift with your left pinky and\u0015",
 
     # 43 = "- ^ \\と=キーについて…"
-    "- ^ \\と=キーについて…",
+    "About the -^\\= keys...",
 
     # 44 = "-は、"
-    "-は、",
+    "To type -, ",
 
     # 45 = "（-を打て）"
-    "（-を打て）",
+    "(press -)",
 
     # 46 = "^も、"
-    "^も、",
+    "To type ^,",
 
     # 47 = "（^を打て）"
-    "（^を打て）",
+    "(press ^)",
 
     # 48 = "\\も、"
-    "\\も、",
+    "To type \\,",
 
     # 49 = "（\\を打て）"
-    "（\\を打て）",
+    "(press \\)",
 
     # 4a = "=は、"
-    "=は、",
+    "To type =,",
 
     # 4b = "（=を打て）"
-    "（=を打て）",
+    "(press =)",
 
     # 4c = "[ ] { } : *と_キーについて…"
-    "[ ] { } : *と_キーについて…",
+    "About the []{}:*_ keys...",
 
     # 4d = "[は、"
-    "[は、",
+    "To type [,",
 
     # 4e = "（[を打て）"
-    "（[を打て）",
+    "(press [)",
 
     # 4f = "]も、"
-    "]も、",
+    "To type ],",
 
     # 50 = "50"
     "50",
 
     # 51 = "（]を打て）"
-    "（]を打て）",
+    "(press ])",
 
     # 52 = "{は、"
-    "{は、",
+    "To type {,",
 
     # 53 = "（{を打て）"
-    "（{を打て）",
+    "(press {)",
 
     # 54 = "}も、"
-    "}も、",
+    "To type },",
 
     # 55 = "（}を打て）"
-    "（}を打て）",
+    "(press })",
 
     # 56 = ":は、"
-    ":は、",
+    "To type :,",
 
     # 57 = "（:を打て）"
-    "（:を打て）",
+    "(press :)",
 
     # 58 = "*は、"
-    "*は、",
+    "To type *,",
 
     # 59 = "（*を打て）"
-    "（*を打て）",
+    "(press *)",
 
     # 5a = "_は、"
-    "_は、",
+    "To type _,",
 
     # 5b = "（_を打て）"
-    "（_を打て）",
+    "(press _)",
 
     # 5c = "以上。"
-    "以上。",
+    "That is all.",
 
     # 5d = "5D"
-    "5D",
+    "",
 
     # 5e = "5E"
-    "5E",
+    "",
 
     # 5f = "5F"
-    "5F",
+    "",
 
     # 60 = "60"
-    "60",
+    "",
 
     # 61 = "私はタマオ博士だ。\u0011\u0015クムの木と話をする研究をしていたんだが…\u0015挫折してしまったよ。"
-    "私はタマオ博士だ。\u0011\u0015クムの木と話をする研究をしていたんだが…\u0015挫折してしまったよ。",
+    "~I'm Dr. Tamao. I came here to study the keytrees, but I hit a roadblock in my research.",
 
     # 62 = "研究を続けてくれって?\u0011\u0015私だって、出来ればそうしたいさ。 …しかし\u0015研究材料のチャトフィッシュが無くてね。"
-    "研究を続けてくれって?\u0011\u0015私だって、出来ればそうしたいさ。 …しかし\u0015研究材料のチャトフィッシュが無くてね。",
+    "~Continue my studies? Oh, I'd like to. But I don't have the chatfish I need for my experiment.",
 
     # 63 = "ルチュ湖のほとりの、ルチュ村あたりで\u0015売ってるんじゃないかと思うんだが…\u0011\u0015断層やらなにやらで、ちょっと行けないだろ。"
-    "ルチュ湖のほとりの、ルチュ村あたりで\u0015売ってるんじゃないかと思うんだが…\u0011\u0015断層やらなにやらで、ちょっと行けないだろ。",
+    "~They might sell them in Rtyu village, but I can't go there myself, what with those chasms.",
 
     # 64 = "何、チャトフィッシュが手に入ったと!\u0015本当かね。\u0011\u0015何だい、これは… 佃煮じゃないか。"
-    "何、チャトフィッシュが手に入ったと!\u0015本当かね。\u0011\u0015何だい、これは… 佃煮じゃないか。",
+    "~You actually found a chatfish? Unbelievable! ...Oh, wait. This is chatfish soup.",
 
     # 65 = "んんー ま、いいか。\u0011\u0015ちょっとそこで待ってなさい。\u0015…ふむ、やはりこうなっているのか。"
-    "んんー ま、いいか。\u0011\u0015ちょっとそこで待ってなさい。\u0015…ふむ、やはりこうなっているのか。",
+    "~Hmmm... well, whatever. Please wait right there. I see, I see...",
 
     # 66 = "…と言う事は、これとそれとあれを調合すれば\u0015クムの木と話すための薬が出来る筈なんだが、\u0015ここには十分な材料が無いなあ。"
-    "…と言う事は、これとそれとあれを調合すれば\u0015クムの木と話すための薬が出来る筈なんだが、\u0015ここには十分な材料が無いなあ。",
+    "~With the right formula, we could produce a drug that lets you talk to keytrees. But I don't have the ingredients.",
 
     # 67 = "メモを書いてあげるから−\u0015クミエルのクムの木研究所で\u0015調合して貰ってはどうかね。"
-    "メモを書いてあげるから−\u0015クミエルのクムの木研究所で\u0015調合して貰ってはどうかね。",
+    "~I'll write them down for you. Please have the lab in Kumiel synthesize it.",
 
     # 68 = "これで完全とは言えないが−\u0015石化したやつも、話しかければ一時的に\u0015復活するかも知れんよ。"
-    "これで完全とは言えないが−\u0015石化したやつも、話しかければ一時的に\u0015復活するかも知れんよ。",
+    "~Even petrified keytrees may be revived if you talk to them using this pill, albeit temporarily...",
 
     # 69 = "\u0018メモを貰った。"
-    "\u0018メモを貰った。",
+    "\u0018Got the Notes.",
 
     # 6a = "配合のメモかね? ほれ。"
     "配合のメモかね? ほれ。",
 
     # 6b = "４階の奥にいる男なら\u0015そのメモで判ると思うが…。"
-    "４階の奥にいる男なら\u0015そのメモで判ると思うが…。",
+    "~I'm sure the scientist at the back of the 4th floor knows what to do with this.",
 
     # 6c = "花壇を踏んずけないように。"
-    "花壇を踏んずけないように。",
+    "~Keep off the flowers.",
 
     # 6d = "でっかい鍵穴に−\u0015水晶を食べさせてる夢を見ちゃった。\u0015何か特別な意味でもあるのかね。"
-    "でっかい鍵穴に−\u0015水晶を食べさせてる夢を見ちゃった。\u0015何か特別な意味でもあるのかね。",
+    "~I dreamed I was putting crystals in a large keyhole. What could it mean?",
 
     # 6e = "いやー、許してね。軽いジョークだから。\u0015え 薬はうまく出来たって?\u0011\u0015あの メモの配合で?  …???"
-    "いやー、許してね。軽いジョークだから。\u0015え 薬はうまく出来たって?\u0011\u0015あの メモの配合で?  …???",
+    "~Haha, sorry. That was a little prank.\x15Wait, the drug actually works?\u0011\u0015With the notes I gave you? ...???",
 
     # 6f = "6F"
-    "6F",
+    "",
 
     # 70 = "70"
-    "70",
+    "",
 
     # 71 = "え お姫様がさらわれたんじゃないかって?\u0011\u0015どーして お姫様が、\u0015さらわれなきゃいけないんですか。"
-    "え お姫様がさらわれたんじゃないかって?\u0011\u0015どーして お姫様が、\u0015さらわれなきゃいけないんですか。",
+    "~What do you mean, \"why didn't they kidnap the princess\"? You think a prince can't get kidnapped?",
 
     # 72 = "どーせ、私なんか\u0015さらうに値しない お姫様です。"
-    "どーせ、私なんか\u0015さらうに値しない お姫様です。",
+    "~I'm not the kinda princess they'd kidnap, anyway.",
 
     # 73 = "異変でボーイフレンドに会えないから\u0015ただでさえ機嫌が悪いのよ。\u0015変な事言って、刺激しないで。"
-    "異変でボーイフレンドに会えないから\u0015ただでさえ機嫌が悪いのよ。\u0015変な事言って、刺激しないで。",
+    "~I can't see my boyfriend 'cuz of the anomalies, so I'm already ticked off. Don't make things worse!",
 
     # 74 = "え ミドが? ドリームポイントで?\u0011\u0015ドリームポイントって何?\u0015ミドがさらわれちゃったの? ずるい。"
-    "え ミドが? ドリームポイントで?\u0011\u0015ドリームポイントって何?\u0015ミドがさらわれちゃったの? ずるい。",
+    "~Wha? Mido? At the Dreampoint? What's a Dreampoint? So Mido was kidnapped? This sucks.",
 
     # 75 = "違うの? よく判らないわ。\u0011\u0015夢の中から? えーっ!!\u0015全然判わないけど、判ったわ。"
-    "違うの? よく判らないわ。\u0011\u0015夢の中から? えーっ!!\u0015全然判わないけど、判ったわ。",
+    "~No? I don't get it at all. Oh, he's trapped in a dream!? Jeez! That's crazy, but I get it.",
 
     # 76 = "夢で、ミドに帰るように言えばいいのね。\u0015かわいそうなミド!!\u0015私が夢見るまで、待っててね。"
-    "夢で、ミドに帰るように言えばいいのね。\u0015かわいそうなミド!!\u0015私が夢見るまで、待っててね。",
+    "~Maybe dream-me could tell Mido to come back. Oh, poor Mido! I'll come for you in my dream!",
 
     # 77 = "早く助けに行ってあげて。\u0015私これから、眠らなきゃ。"
-    "早く助けに行ってあげて。\u0015私これから、眠らなきゃ。",
+    "~Please go save him! I'll try to sleep.",
 
     # 78 = "私に手伝える事は?\u0011\u0015無い。\u0015良かった。"
-    "私に手伝える事は?\u0011\u0015無い。\u0015良かった。",
+    "~Anything I can do to help?\u0011 No? Whew.",
 
     # 79 = "お姫様をさしおいて、彼の方をさらうなんて\u0015ひどい奴ですね、ドリームポイントって。\u0015え 違うんですか。"
-    "お姫様をさしおいて、彼の方をさらうなんて\u0015ひどい奴ですね、ドリームポイントって。\u0015え 違うんですか。",
+    "~Taking him instead of the princess! What a jerk that Dreampoint is! ...Eh? Do I have it wrong?",
 
     # 7a = "まあ、ミド!! 無事だったのね。\u0015"
-    "まあ、ミド!! 無事だったのね。\u0015",
+    "Oh, Mido!! You're safe!\u0015",
 
     # 7b = "さん、ありがとう。\u0011\u0015私、夢の中で何を話していいか判らなくて…"
-    "さん、ありがとう。\u0011\u0015私、夢の中で何を話していいか判らなくて…",
+    ", thank you so much.\u0011\u0015I didn't know what to say in the dream,",
 
     # 7c = "ずっと歌い続けていたの。\u0011\u0015夢の中で歌うのって、とっても難しかったわ。\u0015それにしても ミドったら−"
-    "ずっと歌い続けていたの。\u0011\u0015夢の中で歌うのって、とっても難しかったわ。\u0015それにしても ミドったら−",
+    "~so I just kept on singing. It's hard to sing in a draem, you know.\x15Oh jeez, Mido!",
 
     # 7d = "こんなに痩せちゃって!!\u0015んー、やっぱり…\u0011\u0015私がさらわれた方が良かったかも。"
-    "こんなに痩せちゃって!!\u0015んー、やっぱり…\u0011\u0015私がさらわれた方が良かったかも。",
+    "~You've lost so much weight! Sigh... It really should've been me who got kidnapped.",
 
     # 7e = "7E"
-    "7E",
+    "",
 
     # 7f = "7F"
-    "7F",
+    "",
 
     # 80 = "80"
-    "80",
+    "",
 
     # 81 = "よくやった。ホントに ホントによくやった。\u0011\u0015クムドール国民全員にかわって\u0015お礼を申し上げたい。"
-    "よくやった。ホントに ホントによくやった。\u0011\u0015クムドール国民全員にかわって\u0015お礼を申し上げたい。",
+    "~You did an outstanding job! On behalf of the people of Kumdor, I thank you.",
 
     # 82 = "私はちょっと、読みかけの本があるので\u0015これで失礼するが…\u0011\u0015それは決して、そなたの今回の活躍を−"
-    "私はちょっと、読みかけの本があるので\u0015これで失礼するが…\u0011\u0015それは決して、そなたの今回の活躍を−",
+    "~Now, I have a book to get back to, so if you will please excuse me...",
 
     # 83 = "軽視しての事ではないので、\u0015そのへんのところ、誤解の無いよう。\u0011\u0015では…。"
-    "軽視しての事ではないので、\u0015そのへんのところ、誤解の無いよう。\u0011\u0015では…。",
+    "~I don't mean to disrespect your achievements, so please don't misunderstand. Well, then.",
 
     # 84 = "ところで…\u0011\u0015ちょっとお前、席を外してくれないか。\u0015「え、何?」"
-    "ところで…\u0011\u0015ちょっとお前、席を外してくれないか。\u0015「え、何?」",
+    "By the way...\u0011\u0015Um, can you go take a coffee break?\u0015\"Huh, why?\"",
 
     # 85 = "何でもないったら。\u0011\u0015ちょっとだけ。\u0015「ぶつ ぶつ…。」"
-    "何でもないったら。\u0011\u0015ちょっとだけ。\u0015「ぶつ ぶつ…。」",
+    "It's really nothing.\u0011\u0015Just humor me.\u0015\"Mumble mumble...\"",
 
     # 86 = "実は… あなたにお会いした時から\u0015ずーーーっと考えていたんだが−\u0015そのう… \u0011照れるなあ、ははは。"
-    "実は… あなたにお会いした時から\u0015ずーーーっと考えていたんだが−\u0015そのう… \u0011照れるなあ、ははは。",
+    "~There's something I've wanted to ask you from the moment we first met.\x15I, um... \x11Oh, I'm blushing.",
 
     # 87 = "私はもう決心したんです。つまりその−\u0015あなたに…\u0011\u0015いざ言うとなるとやっぱり恥ずかしいなあ。"
-    "私はもう決心したんです。つまりその−\u0015あなたに…\u0011\u0015いざ言うとなるとやっぱり恥ずかしいなあ。",
+    "~I've made up my mind, and I'd like to ask you... ahh, it's so embarrassing to actually say.",
 
     # 88 = "ブ ブ ブラインドタッチを\u0015教えてくれませんか。\u0011\u0015ああ、言ってしまった。"
-    "ブ ブ ブラインドタッチを\u0015教えてくれませんか。\u0011\u0015ああ、言ってしまった。",
+    "~W-w-would you please...\x15teach me touch typing?\x11\x15Ahh, there, I said it!",
 
     # 89 = "ダメ? …そうですか。\u0011\u0015じゃあ、「クムドールの剣」でも買ってきて\u0015孤独にやるしかないんですね。"
-    "ダメ? …そうですか。\u0011\u0015じゃあ、「クムドールの剣」でも買ってきて\u0015孤独にやるしかないんですね。",
+    "~No? ...I understand. I'll just have to go buy \"Sword of Kumdor\" and practice on my own.",
 
     # 8a = "よかった。\u0011\u0015どうだミド、君も一緒に弟子入りするか?\u0015「お、お願いします!!」"
-    "よかった。\u0011\u0015どうだミド、君も一緒に弟子入りするか?\u0015「お、お願いします!!」",
+    "~Hooray! Mido, would you like to be my fellow disciple!\x15\"Y-yes please!!\"",
 
     # 8b = "「私もやるわっ。」\u0015えーーーーーーーーーーーーっ!"
-    "「私もやるわっ。」\u0015えーーーーーーーーーーーーっ!",
+    "\"I'll join too!\"\u0015Whaaaaaaa?!",
 
     # 8c = "8C"
-    "8C",
+    "",
 
     # 8d = "8D"
-    "8D",
+    "",
 ]
